d3086900f6459ad43ca491467650c35834cc3bd6
Irdgyu <gilangbayuirwana@gmail.com>
2024-11-06 23:02:08 +0700

    first commit

--- ArrayNilai06.java
+++ ArrayNilai06.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
-
 public class ArrayNilai06 {
     public static void main(String[] args) {
         Scanner input6 = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilali akhir ke-" + i + " : ");
             nilaiAkhir[i] = input6.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            } 
+            else {
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
+            }
         }
         input6.close();
     }
