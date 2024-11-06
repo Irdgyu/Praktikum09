b02765135cbea132cf6b591307c264ee8ba6f97b
Irdgyu <gilangbayuirwana@gmail.com>
2024-11-06 23:29:59 +0700

    first commit

--- ArrayRataNilai06.java
+++ ArrayRataNilai06.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
-
 public class ArrayRataNilai06 {
     public static void main(String[] args) {
         Scanner input6 = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMhs = input6.nextInt();
+
+        int[] nilaiMhs = new int[jumlahMhs];
+        int jumlahMhsLulus = 0, jumlahMhsTdkLulus = 0;
+        double totalMhsLulus= 0, totalMhsTdkLulus = 0;
+        double rata2Lulus, rata2TdkLulus;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = input6.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i ++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                totalMhsLulus += nilaiMhs[i];
+                jumlahMhsLulus++;
+            }
+            else {
+                totalMhsTdkLulus += nilaiMhs[i];
+                jumlahMhsTdkLulus++;
+            }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
-        input6.close();
+        rata2Lulus = totalMhsLulus / jumlahMhsLulus;
+        rata2TdkLulus = totalMhsTdkLulus / jumlahMhsTdkLulus;
+        System.out.println("Rata-rata nilai : " + rata2Lulus);
+        System.out.println("Rata-rata nilai tidak lulus : " + rata2TdkLulus) ;
     }
 }
