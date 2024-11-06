9c17308783083a07209df6f9f5be1869b5e37e53
Irdgyu <gilangbayuirwana@gmail.com>
2024-11-07 00:04:55 +0700

    first commit

--- SearchNilai06.java
+++ SearchNilai06.java
@@ -1,18 +1,33 @@
+import java.util.Scanner;
 public class SearchNilai06 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
+        Scanner input6 = new Scanner(System.in);
+
+        System.out.print("Masukkan banyak nilai yang diinput: ");
+        int banyakNilai = input6.nextInt();
+
+        int[] arrNilai = new int[banyakNilai];
         int key = 90;
         int hasil = 0;
+        boolean cari = false;
 
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiwa ke-" + (i+1) + " : ");
+            arrNilai[i] = input6.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        key = input6.nextInt();
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
                 hasil = i;
+                System.out.println();
+                System.out.println("Nilai yang dicari ketemu, " + key + " merupakan nilai mahasiswa ke-" + (hasil+1));
                 break;
             }
         }
-        
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        if (cari == false) {
+            System.out.println();
+            System.out.println("Nilai tidak ketemu");
+        }
     }
 }
